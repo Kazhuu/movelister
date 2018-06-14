607de4dc8acab221fe080b0f8935d0edb5f146d9
Mauri Mustonen <mauri_mustonen@hotmail.com>
2018-06-14 19:17:04 +0300

    more cursor helper functions

--- pythonpath/movelister/cursor.py
+++ pythonpath/movelister/cursor.py
@@ -1,3 +1,15 @@
+def getUsedAreaSize(sheet):
+    """
+    Returns used sheet area as a CellRangeAddress object.
+    See http://www.openoffice.org/api/docs/common/ref/com/sun/star/table/CellRangeAddress.html
+    for object methods.
+    """
+    cursor = sheet.createCursor()
+    cursor.gotoStartOfUsedArea(False)
+    cursor.gotoEndOfUsedArea(True)
+    return cursor.getRangeAddress()
+
+
 def getSheetContent(sheet):
     """
     Returns two dimensional array of all non empty content of the given
@@ -21,3 +33,18 @@ def setSheetContent(sheet, data):
     cursor.gotoOffset(0, 0)
     cursor.collapseToSize(columns, rows)
     cursor.setDataArray(data)
+
+
+def getRowsContent(sheet, startRow, endRow):
+    """
+    Return given rows data in two dimensional array. Data is just a
+    slice of used sheet space returned by getSheetContent(). So if
+    used space is larger than sliced row content, then empty cells
+    are included in with empty strings.
+    """
+    cursor = sheet.createCursor()
+    cursor.gotoStartOfUsedArea(False)
+    cursor.gotoEndOfUsedArea(True)
+    rangeAddress = cursor.getRangeAddress()
+    # left, top, right, bottom
+    return cursor.getCellRangeByPosition(startRow, rangeAddress.StartRow, endRow, rangeAddress.EndRow).getDataArray()
