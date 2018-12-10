537d393a9b06e3d2b9444e4cc457739229aef401
Mauri Mustonen <mauri_mustonen@hotmail.com>
2018-12-10 14:36:45 +0200

    test fix

--- test/format/test_generate.py
+++ test/format/test_generate.py
@@ -5,8 +5,8 @@ from movelister.sheet import Master, Overview
 
 class GenerateTestCase(OfficeTestCase):
 
-    def testDrawOverview(self):
+    def testGenerateOverview(self):
         master = Master('Master List')
         overview = Overview('Test overview')
         overview.setActions(master.getActions())
-        generate.drawSheet(overview, 0)
+        generate.generateSheet(overview, 0)
