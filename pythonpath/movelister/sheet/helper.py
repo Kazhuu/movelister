3d3441293475c7cb885c65e7616cdde298e737a0
Mauri Mustonen <mauri_mustonen@hotmail.com>
2018-12-10 18:51:46 +0200

    helper refactor

--- pythonpath/movelister/sheet/helper.py
+++ pythonpath/movelister/sheet/helper.py
@@ -1,12 +1,11 @@
 from movelister import error
-from movelister.core import cursor
-from movelister.ui import messageBox
 
 
 def getHeaderRowPosition(sheetData):
     """
-    This function figures out at what position the header row is. This may vary between sheets because of UI.
-    The code is simple and basically looks for the first non-empty cell on second column.
+    This function figures out at what position the header row is. This may vary
+    between sheets because of UI. The code is simple and basically looks for
+    the first non-empty cell on second column.
     """
     for index, row in enumerate(sheetData):
         if row[1] != '':
@@ -16,49 +15,23 @@ def getHeaderRowPosition(sheetData):
 
 def getColumnPosition(sheetData, columnName):
     """
-    This function iterates through the header row of a List to find where a chosen Column is.
+    This function iterates through the header row and return given column name
+    index.
     """
-    headerRowPosition = getHeaderRowPosition(sheetData)
-
-    columnRow = sheetData[headerRowPosition]
-    column = -1
-
-    x = -1
-    while x < len(columnRow) - 1:
-        x = x + 1
-        if columnRow[x] == columnName:
-            column = x
-            break
-
-    # Error message if it wasn't found.
-    if column == - 1:
-        msg = "Program couldn't find the column {0}.".format(columnName)
-        messageBox.createMessage('OK', 'Warning:', msg)
-        exit()
-
-    return column
+    headerRow = getHeaderRowPosition(sheetData)
+    columnRow = sheetData[headerRow]
+    return columnRow.index(columnName)
 
 
-def getRowPosition(sheet, text, column):
+def getRowPosition(sheetData, text, column):
     """
-    This function iterates through a column to find the location of a cell that contains a chosen string.
+    This function iterates through a column to find the location of a cell that
+    contains a chosen string.
     """
-    mda = cursor.getSheetContent(sheet)
-    row = -1
-
-    x = -1
-    while x < len(mda) - 1:
-        x = x + 1
-        if mda[x][column] == text:
-            row = x
-            break
-
-    # Error message if it wasn't found.
-    if row == - 1:
-        messageBox.createMessage('OK', 'Warning:', "Program couldn't find where is " + text + '.')
-        exit()
-
-    return row
+    for index, row in enumerate(sheetData):
+        if row[column] == text:
+            return index
+    raise ValueError('{0} is not in column {1}'.format(text, column))
 
 
 def getCellColorsFromColumn(sheet, column, top, bottom):
