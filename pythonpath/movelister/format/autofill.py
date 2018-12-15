29c5f541d35da35621e419e8a787c7066980c541
Nelitarnia <hanzu1988@hotmail.com>
2018-12-15 22:04:01 +0200

    added "autofill" module. Listed some ideas on its use.

---
+++ pythonpath/movelister/format/autofill.py
@@ -0,0 +1,30 @@
+def autoFillMasterList():
+    '''
+    A function that goes through the 'name' column of Master List. When finding a name, it will
+    check if other fields on the same row have empty fields to them. If View or Input List fields
+    are empty, the function adds 'Default'. If Phases field is empty, the function adds 1.
+    '''
+    print()
+
+
+def autoFillOverview():
+    '''
+    A function that checks if the Phases column is correctly filled in an Overview before generating.
+    '''
+    print()
+
+
+def autoFillInputs():
+    '''
+    A function that checks if Input List column is properly filled in Inputs before generating.
+    If a row has a Name but no Input List, then the latter field is filled with 'Default'.
+    '''
+    print()
+
+
+def generateDefaultInputs():
+    '''
+    A function that creates an Input List called 'Default' if one doesn't exist in the project
+    for a reason or another.
+    '''
+    print()
