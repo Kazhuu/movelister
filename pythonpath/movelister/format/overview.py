b4e2fa434bbb9fe3c993afd5bb0d79e103fad57d
Mauri Mustonen <mauri_mustonen@hotmail.com>
2020-01-05 17:30:18 +0200

    remove overview template header

--- pythonpath/movelister/format/overview.py
+++ pythonpath/movelister/format/overview.py
@@ -24,9 +24,7 @@ class OverviewFormatter:
         formatted Overview class instance data in it.
         """
         sheet = Sheet.newOverview(self.instance.name)
-        data = self.format()
-        data = self._copyOverviewHeaderFromTemplate(sheet, data)
-        cursor.setSheetContent(sheet, data)
+        cursor.setSheetContent(sheet, self.format())
         return sheet
 
     def format(self):
@@ -92,14 +90,3 @@ class OverviewFormatter:
                     startCol + x - offset, headerRow, startCol + x, columnLength - headerRow
                     ).CellBackColor = currentColor.value
                 offset = 0
-
-    def _copyOverviewHeaderFromTemplate(self, sheet, data):
-        """
-        Copy header from given overview template sheet and place it into new
-        overview data. This is needed after formatted overview because overview
-        class doesn't hold this information.
-        """
-        for index, cell in enumerate(cursor.getRow(sheet, 0)):
-            data[0][index] = cell
-        cursor.setSheetContent(sheet, self.format())
-        return data
