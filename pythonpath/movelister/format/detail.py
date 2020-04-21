997865935917d20d87bcf8b531a5f22bee694d7f
Nelitarnia <hanzu1988@hotmail.com>
2020-04-21 22:37:52 +0300

    small changes

--- pythonpath/movelister/format/detail.py
+++ pythonpath/movelister/format/detail.py
@@ -11,9 +11,11 @@ class DetailFormatter:
 
     def format(self):
         data = []
+        for line in self.detail:
+            data.append()
         return data
 
-    def _formatRow(self, phase):
+    def _formatRow(self):
         row = []
         # Action Name
         row.append(self.detail.action)
@@ -21,6 +23,8 @@ class DetailFormatter:
         row.append(self.detail.modifiers)
         # Input to Compare
         row.append(self.detail.inputs)
-        # Phase 0
+        # Phases TO DO
         row.append(self.detail.phases[self.detail.inputs][0])
+        # Notes TO DO
+        row.append(self.detail.notes[self.detail.inputs][0])
         return row
